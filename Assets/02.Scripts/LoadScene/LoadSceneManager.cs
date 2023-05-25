6b63ba83f6dc43b698763474b88c99ee29663fbb
Jemin087 <58579317+Jemin087@users.noreply.github.com>
2023-05-26 08:12:59 +0900

    풀,물 타일 추가

--- Assets/02.Scripts/LoadScene/LoadSceneManager.cs
+++ Assets/02.Scripts/LoadScene/LoadSceneManager.cs
@@ -9,44 +9,18 @@ public class LoadSceneManager : MonoBehaviour
     [SerializeField]
     string sceneName;
 
-    [SerializeField]
-    Image  bar;
+ 
 
-    // Start is called before the first frame update
-    void Start()
+    public void GameStart()
     {
-       StartCoroutine(LoadScene(sceneName));
+        StartCoroutine(LoadScene(sceneName));
     }
 
      IEnumerator LoadScene(string name)
      {
         AsyncOperation asyncOper=SceneManager.LoadSceneAsync(name);
-        asyncOper.allowSceneActivation=false;
-        float timer=0.0f;
-        while(!asyncOper.isDone)
-        {
-            yield return null;
-            timer+=Time.deltaTime;
-            if(asyncOper.progress<=0.9f)
-            {
-                bar.fillAmount=Mathf.Lerp(bar.fillAmount,asyncOper.progress,timer);
-                if(bar.fillAmount>=asyncOper.progress)
-                {
-                     timer=0f;
-                }
-               
-            }
-            else
-            {
-               bar.fillAmount=Mathf.Lerp(bar.fillAmount,1f,timer);
-               if(bar.fillAmount==1.0f)
-               {
-                  asyncOper.allowSceneActivation=true;
-                  yield break;
-               }
 
-            }
-        }
+        yield return null;
 
      }
 
--- Assets/02.Scripts/Map/MapGenerate.cs
+++ Assets/02.Scripts/Map/MapGenerate.cs
@@ -36,7 +36,7 @@ public class MapGenerate : MonoBehaviour
     float maxDevideRate;    //최대로 나눌 크기 현재 0.6
 
     [SerializeField]        //line,map,roomLine 현재 어떤식으로 방이 나누어져있고 길이 이어져있는지 알기 위해 만든 lineRender용 prefab
-    GameObject line;    
+    GameObject line;
 
     [SerializeField]
     GameObject line2;
@@ -57,16 +57,26 @@ public class MapGenerate : MonoBehaviour
     Tile backgroundTile;          //타일맵 테스트중 
 
     [SerializeField]
-    Tile[] tiles;           //랜덤한 방타일
+    Tile[] groundTiles;
 
-    public bool debugflag=false;
+    [SerializeField]
+    Tile[] roomTiles;
+
+    [SerializeField]
+    Tile[] wallTiles;
+
+    [SerializeField]
+    Tile[] objectTiles;
+
+
+    public bool debugflag = false;
     Node root;
 
 
 
     private void Start()
     {
-        root  = new Node(new RectInt(0, 0, mapSize.x, mapSize.y));
+        root = new Node(new RectInt(0, 0, mapSize.x, mapSize.y));
 
         if (!debugflag)
         {
@@ -77,49 +87,40 @@ public class MapGenerate : MonoBehaviour
         }
     }
 
-   
+
     private void Update()
     {
-        if(debugflag)
+        if (debugflag)
         {
-            if(Input.GetKeyDown(KeyCode.Alpha1))
+            if (Input.GetKeyDown(KeyCode.Alpha1))
             {
                 DrawTileBackGround();
-                 //DrawMap(0, 0);
+                //DrawMap(0, 0);
             }
-            else if(Input.GetKeyDown(KeyCode.Alpha2))
+            else if (Input.GetKeyDown(KeyCode.Alpha2))
             {
-                Divide(root,0);
+                Divide(root, 0);
             }
-            else if(Input.GetKeyDown(KeyCode.Alpha3))
+            else if (Input.GetKeyDown(KeyCode.Alpha3))
             {
-                GenerateRoom(root,0);
+                GenerateRoom(root, 0);
             }
-            else if(Input.GetKeyDown(KeyCode.Alpha4))
+            else if (Input.GetKeyDown(KeyCode.Alpha4))
             {
-                GenerateLoad(root,0);
+                GenerateLoad(root, 0);
             }
         }
     }
 
 
-    void DrawMap(int x,int y) //드로우맵에서 우선 전체맵크기를 보여줍니다 아까 분홍색 사각형
+    void Divide(Node tree, int n) //사각형을 위에서 설정한 비율로 나누어줍니다 0.4/0.6 비율 (재귀적으로)
     {
-        LineRenderer lineRenderer = Instantiate(map).GetComponent<LineRenderer>();
-        lineRenderer.SetPosition(0, new Vector2(x, y) - mapSize / 2);
-        lineRenderer.SetPosition(1, new Vector2(x+mapSize.x, y) - mapSize / 2);
-        lineRenderer.SetPosition(2, new Vector2(x+mapSize.x, y+mapSize.y) - mapSize / 2);
-        lineRenderer.SetPosition(3, new Vector2(x, y+mapSize.y) - mapSize / 2);
-    }
-
-    void Divide(Node tree,int n) //사각형을 위에서 설정한 비율로 나누어줍니다 0.4/0.6 비율 (재귀적으로)
-    { 
         if (n == maxDepth) return;
 
         int maxLength = Mathf.Max(tree.nodeRect.width, tree.nodeRect.height);   //가로 세로 중 더 긴것으로 나누어주기위해
         int split = Mathf.RoundToInt(Random.Range(maxLength * minDevideRate, maxLength * maxDevideRate)); //나올수 있는 최대길이와 최소길이 중 랜덤으로선택
 
-        if(tree.nodeRect.width>=tree.nodeRect.height)   //가로가 더 길경우 좌/우로 
+        if (tree.nodeRect.width >= tree.nodeRect.height)   //가로가 더 길경우 좌/우로 
         {
             tree.leftNode = new Node(new RectInt(tree.nodeRect.x, tree.nodeRect.y, split, tree.nodeRect.height));
             tree.rightNode = new Node(new RectInt(tree.nodeRect.x + split, tree.nodeRect.y, tree.nodeRect.width - split, tree.nodeRect.height));
@@ -129,38 +130,24 @@ public class MapGenerate : MonoBehaviour
         }
         else        //세로 
         {
-            tree.leftNode = new Node(new RectInt(tree.nodeRect.x, tree.nodeRect.y, tree.nodeRect.width,split));
+            tree.leftNode = new Node(new RectInt(tree.nodeRect.x, tree.nodeRect.y, tree.nodeRect.width, split));
             tree.rightNode = new Node(new RectInt(tree.nodeRect.x, tree.nodeRect.y + split, tree.nodeRect.width, tree.nodeRect.height - split));
 
-             //DrawLine(new Vector2(tree.nodeRect.x, tree.nodeRect.y + split), new Vector2(tree.nodeRect.x + tree.nodeRect.width, tree.nodeRect.y + split));
-             DrawTileWall(new Vector2Int(tree.nodeRect.x, tree.nodeRect.y + split), new Vector2Int(tree.nodeRect.x + tree.nodeRect.width, tree.nodeRect.y + split));
+            //DrawLine(new Vector2(tree.nodeRect.x, tree.nodeRect.y + split), new Vector2(tree.nodeRect.x + tree.nodeRect.width, tree.nodeRect.y + split));
+            DrawTileWall(new Vector2Int(tree.nodeRect.x, tree.nodeRect.y + split), new Vector2Int(tree.nodeRect.x + tree.nodeRect.width, tree.nodeRect.y + split));
         }
         tree.leftNode.parNode = tree;
         tree.rightNode.parNode = tree;
         Divide(tree.leftNode, n + 1);
-        Divide(tree.rightNode,n + 1);
-    }
-
-    void DrawLine(Vector2 from, Vector2 to)
-    {
-        LineRenderer line = Instantiate(this.line).GetComponent<LineRenderer>();
-        line.SetPosition(0, from - mapSize / 2);
-        line.SetPosition(1, to - mapSize / 2);
-    }
-
-    void DrawLine_2(Vector2 from, Vector2 to)
-    {
-        LineRenderer line = Instantiate(this.line2).GetComponent<LineRenderer>();
-        line.SetPosition(0, from - mapSize / 2);
-        line.SetPosition(1, to - mapSize / 2);
+        Divide(tree.rightNode, n + 1);
     }
 
 
-    RectInt GenerateRoom(Node tree,int n) //나누어준 사각형안에 방을 생성해줍니다.
+    RectInt GenerateRoom(Node tree, int n) //나누어준 사각형안에 방을 생성해줍니다.
     {
         RectInt rect;
 
-        if(n==maxDepth)
+        if (n == maxDepth)
         {
             rect = tree.nodeRect;
             int width = Random.Range(rect.width / 2, rect.width - 1);
@@ -184,82 +171,131 @@ public class MapGenerate : MonoBehaviour
         return rect;
     }
 
-    void GenerateLoad(Node tree,int n) //방생성 후 노드를 거슬러올라가 길연결을해줍니다 파란색 줄
+    void GenerateLoad(Node tree, int n) //방생성 후 노드를 거슬러올라가 길연결을해줍니다
     {
-        if(n==maxDepth) return;
+        if (n == maxDepth) return;
 
-        Vector2Int leftCenter=tree.leftNode.center;
-        Vector2Int rightCenter=tree.rightNode.center;
+        Vector2Int leftCenter = tree.leftNode.center;
+        Vector2Int rightCenter = tree.rightNode.center;
 
         //DrawLine_2(new Vector2(leftCenter.x,leftCenter.y),new Vector2(rightCenter.x,leftCenter.y));
         //DrawLine_2(new Vector2(rightCenter.x,leftCenter.y),new Vector2(rightCenter.x,rightCenter.y));
         DrawTileLine(new Vector2Int(leftCenter.x, leftCenter.y), new Vector2Int(rightCenter.x, leftCenter.y));
         DrawTileLine(new Vector2Int(rightCenter.x, leftCenter.y), new Vector2Int(rightCenter.x, rightCenter.y));
 
-        GenerateLoad(tree.leftNode,n+1);
-        GenerateLoad(tree.rightNode,n+1);
+        GenerateLoad(tree.leftNode, n + 1);
+        GenerateLoad(tree.rightNode, n + 1);
+        //if (n == 0)
+        //{
+        //    Vector2Int entranceLeft = new Vector2Int(leftCenter.x, leftCenter.y);
+        //    Vector2Int entranceRight = new Vector2Int(rightCenter.x, leftCenter.y);
+
+        //    DrawTileLine(new Vector2Int(leftCenter.x, leftCenter.y), entranceLeft);
+        //    DrawTileLine(new Vector2Int(rightCenter.x, leftCenter.y), entranceRight);
+        //}
 
     }
-    void DrawRectangle(RectInt rect)
+    void DrawTileLine(Vector2Int from, Vector2Int to)
     {
-        LineRenderer line = Instantiate(roomLine).GetComponent<LineRenderer>();
-        line.SetPosition(0, new Vector2(rect.x, rect.y) - mapSize / 2);
-        line.SetPosition(1, new Vector2(rect.x + rect.width, rect.y) - mapSize / 2);
-        line.SetPosition(2, new Vector2(rect.x + rect.width, rect.y + rect.height) - mapSize / 2);
-        line.SetPosition(3, new Vector2(rect.x, rect.y + rect.height) - mapSize / 2);
-    }
-
-    void DrawTileLine(Vector2Int from,Vector2Int to)
-    {
-    
-        for(int x=from.x; x<to.x; x++)
+        for (int x = from.x; x < to.x; x++)
         {
-            tilemap.SetTile(new Vector3Int(x - mapSize.x / 2, to.y - mapSize.y / 2, 0), tiles[1]);
+            tilemap.SetTile(new Vector3Int(x - mapSize.x / 2, to.y - mapSize.y / 2, 0), groundTiles[0]);
         }
         for (int y = from.y; y < to.y; y++)
         {
-            tilemap.SetTile(new Vector3Int(to.x - mapSize.x / 2, y - mapSize.y / 2, 0), tiles[1]);
+            tilemap.SetTile(new Vector3Int(to.x - mapSize.x / 2, y - mapSize.y / 2, 0), groundTiles[0]);
         }
 
     }
+
 
     void DrawTileWall(Vector2Int from, Vector2Int to)
     {
         for (int x = from.x; x < to.x; x++)
         {
-            tilemap.SetTile(new Vector3Int(x - mapSize.x / 2, to.y - mapSize.y / 2, 0), tiles[2]);
+            tilemap.SetTile(new Vector3Int(x - mapSize.x / 2, to.y - mapSize.y / 2, 0), wallTiles[0]);
         }
         for (int y = from.y; y < to.y; y++)
         {
-            tilemap.SetTile(new Vector3Int(to.x - mapSize.x / 2, y - mapSize.y / 2, 0), tiles[2]);
+            tilemap.SetTile(new Vector3Int(to.x - mapSize.x / 2, y - mapSize.y / 2, 0), wallTiles[1]);
         }
     }
 
     void DrawTileRoom(RectInt rect)
     {
-        for(int x=rect.x; x<rect.width+rect.x; x++)
+        int grassTileCount = 0;
+        int waterTileCount = 1;
+        for (int x = rect.x; x < rect.width + rect.x; x++)
         {
-            for(int y=rect.y; y<rect.height+rect.y; y++)
+            for (int y = rect.y; y < rect.height + rect.y; y++)
             {
-                if (rect.x == x||rect.y==y||rect.width+rect.x-1==x||rect.height+rect.y-1==y)
+                if (rect.x == x || rect.y == y || rect.width + rect.x - 1 == x || rect.height + rect.y - 1 == y)
                 {
-                    tilemap.SetTile(new Vector3Int(x - mapSize.x / 2, y - mapSize.y / 2, 0), tiles[2]);
+                    tilemap.SetTile(new Vector3Int(x - mapSize.x / 2, y - mapSize.y / 2, 0), roomTiles[0]);
                 }
                 else
                 {
-                    tilemap.SetTile(new Vector3Int(x - mapSize.x / 2, y - mapSize.y / 2, 0), tiles[0]);
+                    tilemap.SetTile(new Vector3Int(x - mapSize.x / 2, y - mapSize.y / 2, 0), roomTiles[1]);
                 }
             }
         }
+
+        for (int x = rect.xMin + 1; x < rect.xMax; x++)
+        {
+            for (int y = rect.yMin + 1; y < rect.yMax; y++)
+            {
+
+                if ((x == rect.xMin + 1 || x == rect.xMax - 2) && y == rect.yMax - 1) //사각형의 위쪽벽부분
+                {
+                    float chance = Random.Range(0f, 1f);
+
+                    if (chance < 0.3f)
+                    {
+                        tilemap.SetTile(new Vector3Int(x - mapSize.x / 2, y - mapSize.y / 2, 0), objectTiles[1]);
+
+                        if(waterTileCount<3)
+                        {
+                            for (int i = 1; i < 4; i++)
+                            {
+                                tilemap.SetTile(new Vector3Int(x - mapSize.x / 2, y - mapSize.y / 2 - i, 0), objectTiles[2]);
+                            }
+                            waterTileCount++;
+                        }
+                       
+                       
+
+                    }
+                }
+                else
+                {
+                    if(x == rect.xMin + 1 || x == rect.xMax - 1 || y == rect.yMin + 1 || y == rect.yMax - 1)
+                    {
+                        continue;
+                    }
+                    float chance = Random.Range(0f, 1f);
+                    if (chance < 0.08f&&grassTileCount<10)
+                    {
+                        grassTileCount++;
+                        tilemap.SetTile(new Vector3Int(x - mapSize.x / 2, y - mapSize.y / 2, 0), objectTiles[0]);
+                    }
+
+                    if(grassTileCount==9)
+                    {
+                        grassTileCount = 0;
+                    }
+                }
+            }
+        }
+
     }
 
     void DrawTileBackGround()
-    {      
-        for(int x=-mapSize.x/2; x<mapSize.x/2; x++)
+    {
+        for (int x = -mapSize.x / 2; x < mapSize.x / 2; x++)
         {
-            for(int y=-mapSize.y/2; y<mapSize.y/2; y++)
+            for (int y = -mapSize.y / 2; y < mapSize.y / 2; y++)
             {
-                tilemap.SetTile(new Vector3Int(x,y,0),backgroundTile);
+                tilemap.SetTile(new Vector3Int(x, y, 0), backgroundTile);
             }
         }
 
